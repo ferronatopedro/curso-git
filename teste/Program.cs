1a2842887bd3ca997451c3d468c95009ed04960d
Pedro Ferronato <pedroferronato@outlook.com>
2021-04-15 21:02:08 -0300

    Acrescentada uma mensagem de bom dia

--- teste/Program.cs
+++ teste/Program.cs
@@ -7,6 +7,9 @@ namespace teste
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("bom dia");
+
+            Console.ReadLine();
         }
     }
 }
